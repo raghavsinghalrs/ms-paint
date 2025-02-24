af27ebff8e1a48f1a1991ec5d6e384bbc145a5b3
Raghav Singhal <84004096+raghavsinghalrs@users.noreply.github.com>
2025-02-24 21:24:11 +0530

    canvas added

---
+++ src/app/canvas/canvas.component.ts
@@ -0,0 +1,82 @@
+import { Component, ElementRef, ViewChild, AfterViewInit, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-canvas',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './canvas.component.html',
+  styleUrls: ['./canvas.component.scss']
+})
+export class CanvasComponent implements AfterViewInit {
+  @ViewChild('paintCanvas', { static: false }) canvasRef!: ElementRef<HTMLCanvasElement>;
+  @Input() currentColor : string = '';
+  private ctx!: CanvasRenderingContext2D | null;
+  private isDrawing = false;
+  brushColor: string = '#000000';
+  backgroundColor: string = '#000000';
+  brushSize: number = 5;
+
+  ngAfterViewInit() {
+    const canvas = this.canvasRef.nativeElement;
+    this.ctx = canvas.getContext('2d');
+
+    if (this.ctx) {
+      canvas.width = window.innerWidth - 40;
+      canvas.height = 500;
+      this.setCanvasBackground('#ffffff');
+      this.setupCanvas();
+    }
+  }
+
+  setCanvasBackground(color: string) {
+    if (this.ctx) {
+      this.ctx.fillStyle = color;
+      this.ctx.fillRect(0, 0, this.canvasRef.nativeElement.width, this.canvasRef.nativeElement.height);
+    }
+  }
+
+  
+
+  private setupCanvas() {
+    if (!this.ctx) return;
+    
+    this.ctx.lineCap = 'round';
+    this.ctx.lineWidth = this.brushSize;
+    this.ctx.strokeStyle = this.brushColor;
+    this.ctx.fillStyle = this.backgroundColor;
+    this.ctx.fillRect(0, 0, this.canvasRef.nativeElement.width, this.canvasRef.nativeElement.height);
+
+    const canvas = this.canvasRef.nativeElement;
+    canvas.addEventListener('mousedown', (event) => this.startDrawing(event));
+    canvas.addEventListener('mousemove', (event) => this.draw(event));
+    canvas.addEventListener('mouseup', () => this.stopDrawing());
+    canvas.addEventListener('mouseleave', () => this.stopDrawing());
+  }
+
+  private startDrawing(event: MouseEvent) {
+    this.isDrawing = true;
+    this.draw(event);
+  }
+
+  private draw(event: MouseEvent) {
+    if (!this.isDrawing || !this.ctx) return;
+
+    this.ctx.lineWidth = this.brushSize;
+    this.ctx.strokeStyle = this.brushColor;
+    this.ctx.lineTo(event.offsetX, event.offsetY);
+    this.ctx.stroke();
+    this.ctx.beginPath();
+    this.ctx.moveTo(event.offsetX, event.offsetY);
+  }
+
+  private stopDrawing() {
+    this.isDrawing = false;
+    if (this.ctx) this.ctx.beginPath();
+  }
+
+  clearCanvas() {
+    if (this.ctx) {
+      this.ctx.clearRect(0, 0, this.canvasRef.nativeElement.width, this.canvasRef.nativeElement.height);
+    }
+  }
+}
